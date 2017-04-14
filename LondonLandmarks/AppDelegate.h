bdf2109de6665425f47267db262582910ee805b0
James Volmert <james.volmert@gmail.com>
2017-04-14 15:03:19 -0600

    Initial Commit

---
+++ LondonLandmarks/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LondonLandmarks
+//
+//  Created by James Volmert on 4/14/17.
+//  Copyright © 2017 James Volmert. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LondonLandmarks/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LondonLandmarks
+//
+//  Created by James Volmert on 4/14/17.
+//  Copyright © 2017 James Volmert. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
